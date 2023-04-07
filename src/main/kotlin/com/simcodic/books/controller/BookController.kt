1d4b3414eff68d8f8cdd0633b42643af6c27ca1d
Rafal Halasa <rafal.halasa@gmail.com>
2023-04-07 08:32:51 +0200

    change properties

--- src/main/kotlin/com/simcodic/books/controller/BookController.kt
+++ src/main/kotlin/com/simcodic/books/controller/BookController.kt
@@ -6,7 +6,7 @@ import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/books", produces = [MediaType.APPLICATION_JSON_VALUE], consumes = [MediaType.APPLICATION_JSON_VALUE])
+@RequestMapping("/books", produces = [MediaType.APPLICATION_JSON_VALUE])
 @EnableAutoConfiguration
 class BookController {
 
