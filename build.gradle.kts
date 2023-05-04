441234a980639d9082b0df8bbb99d811388b7c6d
“rafalhalasa” <rafal.halasa@gmail.com>
2023-05-04 11:43:27 +0200

    add user and authentication

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework:spring-web")
     implementation("org.springframework:spring-context")
     implementation("org.springframework:spring-aop")
@@ -29,7 +31,7 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
     testImplementation("io.kotest:kotest-property:5.5.5")
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
-    runtimeOnly ("com.mysql:mysql-connector-j")
+    runtimeOnly("com.mysql:mysql-connector-j")
 }
 
 tasks.withType<KotlinCompile> {
